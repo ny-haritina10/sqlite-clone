dcfb0c73f01dc9ee882427576034b79942f8431e
DaxNH10 <daxnh10@gmail.com>
2024-09-06 01:17:46 +0100

    feat: create the REPL component

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import db.component.REPL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        REPL console = new REPL();
+        console.run();
     }   
 }
